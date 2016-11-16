5b1575cccab4a19849b8b40be35bd97d15624c37
amyuta <amy.barnett@mavs.uta.edu>
2016-11-15 19:27:18 -0600

    mshfbv

--- locomotor.cpp
+++ locomotor.cpp
@@ -18,3 +18,19 @@ void Locomotor:: save_all() {
 
             ofs.close();
 }
+
+void Locomotor::save_as_all(string filename) {
+
+	ofstream ofs;
+	ofs.open(filename.c_str(), ofstream::out | ofstream::app);
+
+	ofs << p_num << endl;
+	ofs << p_weight << endl;
+	ofs << p_cost << endl;
+	ofs << p_quantity << endl;
+	ofs << p_power << endl;
+	ofs << p_maxspeed << endl;
+	ofs << p_description << endl;
+
+	ofs.close();
+}
